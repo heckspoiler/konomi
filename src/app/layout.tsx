5cd1446c0377d96be0e9c0818c9440b0c05c6f71
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2025-01-09 22:09:10 +0100

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script';
 
 import Background from './components/background/Background';
 
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 
 import './globals.css';
 import Logo from './components/Logo/Logo';
