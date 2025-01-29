110368f5465b63877df181abbd16368f676784da
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2025-01-29 14:10:35 +0100

    added newsletter form in footer as well, adjustments needed

--- src/app/components/Footer/components/FooterContent.tsx
+++ src/app/components/Footer/components/FooterContent.tsx
@@ -1,56 +1,74 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './FooterContent.module.css';
 import { PrismicRichText } from '@prismicio/react';
 import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import Heart from '../../heart/Heart';
+import NewsletterForm from '../../Homepage/AboutSection/NewsletterForm';
 
 export default function FooterContent({ data }: { data: any }) {
+  const [newsletterIsOpen, setNewsletterIsOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.upperContent}>
-        <PrismicRichText field={data.footer_title} />
-        <PrismicRichText field={data.footer_subtitle} />
-        <div className={styles.japanese}>
-          <PrismicRichText field={data.japanese_subtitle_first} />
-          <PrismicRichText field={data.japanese_subtilte_second} />
+    <>
+      {newsletterIsOpen && (
+        <div className={styles.newsletterOverlay}>
+          <NewsletterForm />
         </div>
-      </div>
-      <div className={styles.lowerContent}>
-        <div className={styles.contact}>
-          <div className={styles.address}>
-            <PrismicRichText field={data.address_street} />
-            <PrismicRichText field={data.address_postcode} />
-          </div>
-          <div className={styles.socials}>
-            {data.socials_links.map((item: any, index: number) => (
-              <div className={styles.socialItem} key={index}>
-                <PrismicNextLink field={item.socials_link}>
-                  <p>{item.socials_link.text}</p>
-                  <PrismicNextImage field={item.socials_icon} />
-                </PrismicNextLink>
-              </div>
-            ))}
+      )}
+      <div className={styles.container}>
+        <div className={styles.upperContent}>
+          <PrismicRichText field={data.footer_title} />
+          <PrismicRichText field={data.footer_subtitle} />
+          <div className={styles.japanese}>
+            <PrismicRichText field={data.japanese_subtitle_first} />
+            <PrismicRichText field={data.japanese_subtilte_second} />
           </div>
         </div>
-        <div className={styles.supporters}>
-          <div className={styles.titleContainer}>
-            <Heart width="15" height="15" />
-            <PrismicRichText field={data.supporters_subtitle} />
+        <div className={styles.lowerContent}>
+          <div className={styles.contact}>
+            <div className={styles.address}>
+              <PrismicRichText field={data.address_street} />
+              <PrismicRichText field={data.address_postcode} />
+            </div>
+            <div className={styles.socials}>
+              {data.socials_links.map((item: any, index: number) => (
+                <div
+                  className={styles.socialItem}
+                  key={index}
+                  onClick={() => {
+                    if (item.socials_link.text === 'Newsletter') {
+                      setNewsletterIsOpen(true);
+                    }
+                  }}
+                >
+                  <PrismicNextLink field={item.socials_link}>
+                    <p>{item.socials_link.text}</p>
+                    <PrismicNextImage field={item.socials_icon} />
+                  </PrismicNextLink>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={styles.supportersLogos}>
-            {data.supporters.map((item: any, index: number) => (
-              <div key={index}>
-                <PrismicNextLink field={item.supporters_link}>
-                  <PrismicNextImage field={item.supporters_image} />
-                </PrismicNextLink>
-              </div>
-            ))}
-          </div>
-        </div>{' '}
+          <div className={styles.supporters}>
+            <div className={styles.titleContainer}>
+              <Heart width="15" height="15" />
+              <PrismicRichText field={data.supporters_subtitle} />
+            </div>
+            <div className={styles.supportersLogos}>
+              {data.supporters.map((item: any, index: number) => (
+                <div key={index}>
+                  <PrismicNextLink field={item.supporters_link}>
+                    <PrismicNextImage field={item.supporters_image} />
+                  </PrismicNextLink>
+                </div>
+              ))}
+            </div>
+          </div>{' '}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
